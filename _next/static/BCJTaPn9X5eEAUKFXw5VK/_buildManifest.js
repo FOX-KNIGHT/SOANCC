bb203a7e4751487427dc57a12647be3246b35240
Siddhant Jena <siddhantjena2007@gmail.com>
2026-02-02 21:50:38 +0530

    Updates

--- _next/static/BCJTaPn9X5eEAUKFXw5VK/_buildManifest.js
+++ _next/static/BCJTaPn9X5eEAUKFXw5VK/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/SOANCC//_next/:path+",
-        "destination": "/SOANCC/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
